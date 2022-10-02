c72ede50a81ac85fb78ef012a8e1a69f53848705
gbktosin <104964198+gbktosin@users.noreply.github.com>
2022-10-02 00:45:25 -0400

    Part 2

--- twitter_ios_starter/Twitter/HomeTableTableViewController.swift
+++ twitter_ios_starter/Twitter/HomeTableTableViewController.swift
@@ -12,23 +12,29 @@ class HomeTableTableViewController: UITableViewController {
 
     var tweetArray = [NSDictionary]()
     var numberOfTweet: Int!
-    
     let myRefreshControl = UIRefreshControl()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadTweets()
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
-        tableView.refreshControl = myRefreshControl
-        
+        self.tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 130
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
     }
     
     @objc func loadTweets() {
         numberOfTweet = 20
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let myParams = ["count": numberOfTweet]
-        
-        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: {
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams as [String : Any], success: {
             (tweets: [NSDictionary]) in
             
             self.tweetArray.removeAll()
@@ -37,11 +43,11 @@ class HomeTableTableViewController: UITableViewController {
             }
             
            self.tableView.reloadData()
-           self.refreshControl?.endRefreshing()
+           self.myRefreshControl.endRefreshing()
             
         }, failure: {
             (Error) in
-            print("Could not retreive tweets! oh no!")
+            print("Could not retreive tweets!")
         })
     }
     
@@ -51,7 +57,7 @@ class HomeTableTableViewController: UITableViewController {
         
         let myParams = ["count": numberOfTweet]
         
-        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: {
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams as [String : Any], success: {
             (tweets:[NSDictionary]) in
             
             self.tweetArray.removeAll()
@@ -60,11 +66,10 @@ class HomeTableTableViewController: UITableViewController {
             }
             
             self.tableView.reloadData()
-            //self.refreshControl?.endRefreshing()
             
         }, failure: {
             (Error) in
-            print("Cant find tweets")
+            print("Could not retrieve tweets!")
         })
         
     }
@@ -87,17 +92,26 @@ class HomeTableTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
         
         let user = tweetArray[indexPath.row]["user"] as! NSDictionary
+        
         cell.userNameLabel.text = user["name"] as? String
-        cell.tweetContent.text = tweetArray[indexPath.row]["text"] as! String
+        cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
     
         let imageUrl = URL(string: (user["profile_image_url_https"] as? String)!)
         let data = try? Data(contentsOf: imageUrl!)
         
+        
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        
         return cell
     }
+    
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
--- twitter_ios_starter/Twitter/TweetCellTableViewCell.swift
+++ twitter_ios_starter/Twitter/TweetCellTableViewCell.swift
@@ -16,10 +16,66 @@ class TweetCellTableViewCell: UITableViewCell {
     
     @IBOutlet weak var tweetContent: UILabel!
     
+    @IBOutlet weak var greyFavButton: UIButton!
+    
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    
+    @IBOutlet weak var greyRetButton: UIButton!
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+        if (toBeFavorited) {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: {
+                (error) in
+                print("Favorite did not suceed: \(error)")
+            })
+        } else {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: {
+                (error) in
+                print("Unfavorite did not suceed: \(error)")
+            })
+        }
+    }
+    
+    @IBAction func retweetTweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweetTweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: {
+            (error) in
+            print("Error is retweeting: \(error)")
+        })
+    }
+    
+    func setRetweeted(_ isRetweeted: Bool) {
+        if (isRetweeted) {
+            greyRetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            greyRetButton.isEnabled = false
+        } else {
+            greyRetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            greyRetButton.isEnabled = true
+        }
+    }
+    
+    func setFavorite(_ isFavorited:Bool) {
+        favorited = isFavorited
+        if (favorited) {
+            greyFavButton.setImage(UIImage(named: "favor-icon-red"),
+                                   for: UIControl.State.normal)
+        } else {
+            greyFavButton.setImage(UIImage(named: "favor-icon"),
+                                   for: UIControl.State.normal)
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-    }
+    }    
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
